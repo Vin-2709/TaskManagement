d1087570c714858d2e7ccb466a9ed919f02c3645
Vin-2709 <vin.itku7277@gmail.com>
2025-06-05 12:08:06 +0530

    Update index.js

--- taskManagement/Backend/index.js
+++ taskManagement/Backend/index.js
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config({});
 import connectDB from "./utils/db.js";
 import app from "./src/app.js";
-const url="https://task-management-theta-pied.vercel.app/"
 
-app.listen(url || 5000,()=>{
+
+app.listen("https://task-management-theta-pied.vercel.app/" || 5000,()=>{
     connectDB();
     console.log("server is running ");
 })
